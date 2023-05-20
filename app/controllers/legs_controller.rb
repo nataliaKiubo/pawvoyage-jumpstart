1e0ef7ea36735de149ec078313c77f270511f4e4
Natalia Ramirez <natalia.ramirez@hotmail.com>
2023-05-20 11:35:14 +0200

    added trips and legs controller plus their routes

---
+++ app/controllers/legs_controller.rb
@@ -0,0 +1,29 @@
+class LegsController < ApplicationController
+
+  def index
+    @legs = Leg.all
+  end
+
+  def new
+    @trip = Trip.find(params[:trip_id])
+    @leg = Leg.new
+  end
+
+  def create
+    @leg = Leg.new(leg_params)
+    raise
+    if @leg.save
+      redirect_to @leg
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def leg_params
+    params.require(:leg).permit(:trip, :origin_address, :destination_address, :description, :vehicle, :provider)
+  end
+
+end
